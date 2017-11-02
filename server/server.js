fd106b934703099b1d2413f25429e8fe6a780d4e
Sven Pannewitz <sven.pannewitz@gmail.com>
2017-11-02 09:00:53 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -29,6 +30,32 @@ app.get('/todos', (req, res) => {
         console.log('Unable to load todos', e);
         res.status(400).send(err);
     })
+});
+
+// GET /todos/1234155
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        res.status(400);
+        res.send('Invalid id');
+        return;
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            res.status(404);
+            res.send({});
+            return;
+        }
+
+        res.status(200);
+        res.send({todo});
+    }, (e) => {
+        res.status(400);
+        res.send(e);        
+    });
+    
 });
 
 app.listen(3000, () => {
